4f09e295ddd5fee5b84500c2708f473dd03638c8
Dalton Kinney <daltonckinney@gmail.com>
2023-05-09 09:25:57 -0600

    Added more fitting idk statement

--- chat.py
+++ chat.py
@@ -49,4 +49,4 @@ while True:
             if tag == intent["tag"]:
                 print(f"{bot_name}: {random.choice(intent['responses'])}")
     else:
-        print(f"{bot_name}: I do not understand...")
+        print(f"{bot_name}: you mad tripping dawg, I ain't the clue what you saying")
